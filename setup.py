626359e4bb6eba86d2996fc043afe6be2eafab95
rohit2kh <rohitkhandal1811@gmail.com>
2023-12-05 11:50:49 +0530

    setup and requirement

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+def get_requirements(file:str) -> List[str]:
+    '''
+    this function will return requiremnets (libraries used)
+    '''
+    requirements = []
+    with open(file) as file_object:
+        requirements = file_object.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name = "ML_project",
+    version = '0.0.1',
+    author = "ROhit Khandal",
+    author_email="rohitkhandal1811@gmail.com",
+    packages=find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
